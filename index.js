6dbe4c074ccb9e58f9b8f4c265e880f417cb1c10
John Azzaro <jazzaro@temple.edu>
2018-10-16 13:28:27 -0700

    JavaScript MS Additions

--- index.js
+++ index.js
@@ -2,18 +2,11 @@
 
 const POWERBALL_URL = 'https://data.ny.gov/api/views/d6yy-54nr/rows.json';
 const MEGAMILLIONS_URL = 'https://data.ny.gov/api/views/5xaw-6ayf/rows.json';
+const POWERBALL = "Powerball";
+const MEGAMILLIONS = "Megamillions";
 
 const STORE = {
-    drawings: [
-        {name: "Powerball",
-        date: new Date(),   // new is an object constructor 
-        numbers: [2,3,4,5,6,18,2],
-    },
-    {name: "MegaMillions",
-    date: new Date(),
-    numbers: [2,3,4,5,6,18,2],
-    },
-    ],
+    drawings: [],
     newsItems: [],
 }
 
@@ -42,10 +35,68 @@ function getMegaMillionsDataFromApi(success, error) {
     getDataFromApi(MEGAMILLIONS_URL, success, error);
 }
 
+function getLotteryDataFromApi() {
+    getPowerballDataFromApi(function(response) {
+        const powerBallDrawings = powerBallAdapter(response.data);
+                // Array spread operator - instead of pushing the whole array inside, it pushes all the array items in at once
+        STORE.drawings.push(powerBallDrawings[powerBallDrawings.length - 1]);
+        getMegaMillionsDataFromApi(function(response) {
+        const megaMillionsDrawings = megaMillionsAdapter(response.data)
+        STORE.drawings.push(megaMillionsDrawings[megaMillionsDrawings.length - 1]);
+        displayMainPage(STORE.drawings, STORE.newsItems);
+        });
+    });
+}
+
+
+function megaMillionsAdapter(drawings) {
+    console.log("running megamilions adapter")
+    const dateIndex = 8;
+    const numbersIndex = 9;
+    const megaBallIndex = 10;
+    const multiplierIndex = 11;
+    return drawings.map((drawing) => {
+        const megaBallMultiplier = [drawing[megaBallIndex], drawing[multiplierIndex]];
+        const numbers = drawing[numbersIndex].split(" ")
+        numbers.push(...megaBallMultiplier)
+            return {
+            name: MEGAMILLIONS,
+            date: new Date(drawing[dateIndex]),
+            numbers
+        }
+    });
+}
+
+function powerBallAdapter(drawings) {
+    const dateIndex = 8;
+    const numbersIndex = 9;
+    const multiplierIndex = 10;
+    return drawings.map((drawing) => {
+        const numbers = drawing[numbersIndex].split(" ")
+        numbers.push(drawing[multiplierIndex])
+        return {
+                name: POWERBALL,
+                date: new Date(drawing[dateIndex]),   // makes a new date out of the date string format
+                numbers
+        }
+    });
+}
+
+
+/*
 function getLotteryDataFromApi(powerBallSuccess, megaMillionsSuccess, powerBallError, megaMillionsError) {
     getPowerballDataFromApi(powerBallSuccess, powerBallError);
     getMegaMillionsDataFromApi(megaMillionsSuccess,megaMillionsError);
 }
+*/
+
+/* call the first lottery (first api)
+then we need to set the STORE's to what the api is (powerball, megamillions) this is in the callback.
+then in the callback, we need to call the second api.
+in the second api's callback, we set the STORE's other lottery, then we display on the screen
+
+*/
+
 
 ////// GENERATE  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -98,19 +149,6 @@ function generateNumberSection(drawings) {
     </section>
     `
 }
-
-// for each drawing in drawings, map it on to generatedrawing items
-/* {name: "Powerball",
-    date: 2/3/2018,
-    numbers: [2,3,4,5,6,18,2],
-}
-adapter from api to app (so if the api changes, then you just need to change the adapter)  
-called programming to contract - adjusting at the point of the adapter so you dont violate the contract
-
-** need to write a piece that gets the data form the api and returns the type of object above.  it will ask the adapter for the data and then the adpater will get from the api.
-
-*/
-
 
 function generateDrawingItem(drawing) {
     // 
@@ -169,6 +207,7 @@ function generateNewsItem(newsItem) {
 
 ///// DISPLAY FUNCTIONS /////////////////////////////////////////////////////////////////////////////////////////////
 
+// reuseable - takes a buch of items, runs the generator on the item, and if true adds tot he container, if false replaces the contents of container.
 function appendOrReplace(items, container, generator, append = true) {
     const html = generator(items);
     if (append) {
@@ -178,16 +217,16 @@ function appendOrReplace(items, container, generator, append = true) {
     }
 }
 
-
-
+// takes the data and displays on page
 function displayMainPage(drawings, newsItems) {
     const main = $('main')
     main.empty();  // this empties it out so that we can do a bunch of appends
-    displayNumberSection(drawings, main);
+    displayNumberSection(drawings, main);   // so the "main" slot is basically to display the information
     displayNavSection(drawings, main);
     displayNewsSection(newsItems, main);
 }
 
+// from displaymainpage, main (becomes conatiner because we might want to put it somewhere other than main).  if append is left off, its undefined, but default is true.
 function displayNumberSection(drawings, container, append = true) {
     appendOrReplace(drawings, container, generateNumberSection, append);
 }
@@ -200,13 +239,14 @@ function displayNewsSection(newsItems, container, append = true) {
     appendOrReplace(newsItems, container, generateNewsSection, append);
 }
 
+function displayNumbersList(numbers, container) {
+    container.html(generateNumbersList(numbers));
+}
+
 function displayDrawingItem(drawing, container) {
     // 
 }
 
-function displayNumbersList(numbers, container) {
-    container.html(generateNumbersList(numbers));
-}
 function displayCountDown(drawing, container) {
     //
 }
@@ -223,8 +263,7 @@ function setUpEventHandlers() {
 
 function initalize() {
     setUpEventHandlers();
-    displayMainPage(STORE.drawings, STORE.newsItems);
-
+    getLotteryDataFromApi();
 }
 
 $(initalize);
